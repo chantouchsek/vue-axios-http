e548a9dfad64ee7542b5ae9eabfee8e89a16e815
Touch <s.chantouch@mango-byte.com>
2020-10-03 14:54:09 +0700

    :fire: remove .nuxt from demo

--- demo/.nuxt/vue-api-queries.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueApiQueries, { Validator, BaseProxy } from 'vue-api-queries'
-
-export default function (ctx, inject) {
-  Vue.use(VueApiQueries)
-  ctx.$errors = Validator
-  BaseProxy.$http = ctx.$axios
-  inject('queries', BaseProxy)
-}
