e443ba286c9663c6111d21781f131d63f80071ed
Chantouch <chantouchsek.cs83@gmail.com>
2023-05-24 14:42:35 +0700

    fix: spread options by string

--- nuxt/templates/plugin.js
+++ nuxt/templates/plugin.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import AxiosHttp, { BaseService, Validator } from 'vue-axios-http'
 
-const options = '<%= options %>'
+const resetParameter = '<%= options.resetParameter %>'
+const errorProperty = '<%= options.errorProperty %>'
+const parsedQs = '<%= options.parsedQs %>'
 
-Vue.use(AxiosHttp, options)
+Vue.use(AxiosHttp, { errorProperty, parsedQs, resetParameter })
 
 export default function ({ $axios }, inject) {
   BaseService.$http = $axios
